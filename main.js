c0ed494c514d8e268f74800668159e2171d48d39
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-15 10:10:39 +0100

    made function flashcards

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ document.querySelector('#app').innerHTML = `
   <div>
     <h1>${title}</h1>
     <h2>${tools.getSubtitle()}</h2>
+    <p>${tools.showFlashcard('sky', 'der Himmel')}</p>
+    <p>${tools.showFlashcard('car', 'das Auto')}</p>
+    <p>${tools.showFlashcard('house', 'das Haus')}</p>
     
   </div>
 `;
--- tools.js
+++ tools.js
@@ -13,3 +13,18 @@ export const getTitle = () => {
 export const getSubtitle = () => {
 	return 'a learn site';
 };
+
+/**
+ * showFlashcard
+ * 
+ * showFlashcard(front, back)
+ * 
+ * showFlashcard('house', 'das Haus')
+ * 
+ * front = what is displayed on the front of the flashcard
+ * 
+ * back = what is displayed on the back of the flashcard
+ */
+export const showFlashcard = (front, back) => {
+	return front + ' <span style="color: red">>>></span> ' + back;
+}
